0e9ba51ba6faa5d34269220e38e6e34a1af93a05
rohit <rohitme0091@gmail.com>
2023-04-15 12:16:10 +0530

    done 5 task ||create class insidee the Main

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -16,8 +16,8 @@ public class Main {
     }
     public static void main(String[] args) {
         B obj = new B();
-        obj.meth();
-//        System.out.println(obj.meth());
+
+       System.out.println(obj.meth());
 //        obj.meth();
     }
 }

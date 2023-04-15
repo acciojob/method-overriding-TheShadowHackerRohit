4342c3f4ee8c16aa6253f57823595b93fa97c62b
rohit <rohitme0091@gmail.com>
2023-04-15 11:11:25 +0530

    done 5 task ||create class insidee the Main

--- src/main/java/com/driver/A.java
+++ src/main/java/com/driver/A.java
@@ -1,7 +1,2 @@
 package com.driver;
 
-class A {
-    public String meth(){
-        return "Invoking method from class A";
-    }
-}
--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,10 +1,23 @@
 package com.driver;
 
 public class Main {
+
+    static class A {
+        public String meth(){
+            return "Invoking method from class A";
+
+        }
+    }
+    static class B extends A {
+        @Override
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
     public static void main(String[] args) {
         B obj = new B();
         obj.meth();
-
-        obj.meth();
+//        System.out.println(obj.meth());
+//        obj.meth();
     }
 }
